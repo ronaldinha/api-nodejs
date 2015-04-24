cac0409e084bb406c514b8e529d9c2e4287dedf6
Nicolas <nicolas.levaique@ubleam.com>
2015-04-25 00:12:37 +0200

    gives wrong method/path on 404

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/', require('./controllers'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error(req.method + ' on ' + req.path + ' is not supported');
   err.status = 404;
   next(err);
 });
